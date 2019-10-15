a1df71fd65115fa1317a613095523901cbea9682
Andy Lu <andy@stitchdata.com>
2019-10-15 15:30:10 +0000

    Bump to v0.0.3, update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 0.0.3
+  * Move Transformer initialization "up a level" to remove noise from logging
+
 ## 0.0.2
   * Adjust json schemas for loan_products, deposit_products for custom_field_values and other array/lists
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-mambu',
-      version='0.0.2',
+      version='0.0.3',
       description='Singer.io tap for extracting data from the Mambu 2.0 API',
       author='jeff.huth@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
