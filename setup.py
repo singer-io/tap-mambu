e2c15b7584288f22165b91435b902eef83cd8e02
Jeff Huth <39202799+jeffhuth-bytecode@users.noreply.github.com>
2020-05-29 07:29:38 -0700

    Bump to 1.0.4
    Bump version to 1.0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-mambu',
-      version='1.0.3',
+      version='1.0.4',
       description='Singer.io tap for extracting data from the Mambu 2.0 API',
       author='jeff.huth@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
