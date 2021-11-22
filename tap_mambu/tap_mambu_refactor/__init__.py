7b11dd8524c489d1b5ac181d7ee8532eade8e231
Radu Marinoiu <radu.marinoiu@mambu.com>
2021-11-22 13:31:32 +0200

    ECDDC-476 Testing jira integration

--- tap_mambu/tap_mambu_refactor/__init__.py
+++ tap_mambu/tap_mambu_refactor/__init__.py
@@ -12,8 +12,3 @@ def sync_endpoint_refactor(client, catalog, state, start_date, stream_name,
     generator, processor = generator_class(client), processor_class()
     for record in generator:
         processor.process(record)
-
-        # for child_stream_name, child_endpoint_config in endpoint_config.get("children"):
-        #     child_generator, child_processor = stream_generator_processor_dict[child_stream_name]
-        #     for child_record in child_generator:
-        #         child_processor.process(child_record)
