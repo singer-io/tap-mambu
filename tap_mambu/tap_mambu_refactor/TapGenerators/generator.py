efb3ea0e66e960d7538402af4c8dda8468f85594
Radu Marinoiu <radu.marinoiu@mambu.com>
2021-11-23 11:57:54 +0200

    Implemented some logic, need to rethink inheritance, especially initialization

--- tap_mambu/tap_mambu_refactor/TapGenerators/generator.py
+++ tap_mambu/tap_mambu_refactor/TapGenerators/generator.py
@@ -5,9 +5,31 @@ import requests
 
 
 class TapGenerator:
-    def __init__(self, client):
-        self.buffer: List = list()
-        self.start_date = 0
+    buffer: List
+
+    def __init__(self, stream_name, client, config, endpoint_config):
+        self.stream_name = stream_name
+        self.client = client
+        self.config = config
+        self.endpoint_config = endpoint_config
+        self.__init_buffers()
+        self.__init_bookmarks()
+        self.__init_params()
+
+    def __init_buffers(self):
+        self.buffer = list()
+
+    def __init_bookmarks(self):
+        self.bookmark_query_field = self.endpoint_config.get('bookmark_query_field')
+        self.bookmark_type = self.endpoint_config.get('bookmark_type')
+        self.last_bookmark_value = 0 if self.bookmark_type == "integer" else self.start_date
+
+    def __init_params(self):
+        self.start_date = self.config.get('start_date')
+        self.static_params = self.endpoint_config.get('params', {})
+        self.offset = 0
+        self.limit = self.client.page_size
+        self.params = self.static_params
 
     def __iter__(self):
         self.buffer = self.fetch_batch()
@@ -21,7 +43,12 @@ class TapGenerator:
         return self.buffer.pop(0)
 
     def fetch_batch(self):
-        if not self.buffer:
-            # fetch lm records
-            pass
-        return list()
+        response = self.client.request(
+            method=self.endpoint_config.get('api_method', 'GET'),
+            path=self.endpoint_config.get('path'),
+            version=self.endpoint_config.get('api_version', 'v2'),
+            apikey_type=self.endpoint_config.get('apikey_type', None),
+            params=self.params,
+            endpoint=self.stream_name,
+            json=self.endpoint_config.get('body', None))
+        return response
--- tap_mambu/tap_mambu_refactor/__init__.py
+++ tap_mambu/tap_mambu_refactor/__init__.py
@@ -1,4 +1,5 @@
 from tap_mambu.tap_mambu_refactor.TapGenerators.generator import TapGenerator
+from tap_mambu.tap_mambu_refactor.TapGenerators.loan_accounts_generator import LoanAccountsGenerator
 from tap_mambu.tap_mambu_refactor.TapProcessors.processor import TapProcessor
 
 stream_generator_processor_dict = {
@@ -9,6 +10,13 @@ stream_generator_processor_dict = {
 def sync_endpoint_refactor(client, catalog, state, start_date, stream_name,
                            path, endpoint_config, sub_type):
     generator_class, processor_class = stream_generator_processor_dict[stream_name]
-    generator, processor = generator_class(client), processor_class()
+    generator, processor = generator_class(stream_name=stream_name,
+                                           client=client,
+                                           endpoint_config=endpoint_config), \
+                           processor_class()
     for record in generator:
         processor.process(record)
+
+
+if __name__ == '__main__':
+    LoanAccountsGenerator(None, None, None, None)
