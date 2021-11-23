12d36285406cc379addb8d5dbac3dc480dcb4efc
Radu Marinoiu <radu.marinoiu@mambu.com>
2021-11-23 12:24:36 +0200

    Pushing more changes, will test after this

--- tap_mambu/tap_mambu_refactor/TapGenerators/generator.py
+++ tap_mambu/tap_mambu_refactor/TapGenerators/generator.py
@@ -1,7 +1,10 @@
 import time
-from typing import List
-
 import requests
+
+from typing import List
+from singer import utils
+
+from tap_mambu.transform import transform_json
 
 
 class TapGenerator:
@@ -26,18 +29,21 @@ class TapGenerator:
 
     def __init_params(self):
         self.start_date = self.config.get('start_date')
+        self.time_extracted = None
         self.static_params = self.endpoint_config.get('params', {})
         self.offset = 0
         self.limit = self.client.page_size
         self.params = self.static_params
 
     def __iter__(self):
-        self.buffer = self.fetch_batch()
+        raw_batch = self.fetch_batch()
+        self.buffer = self.transform_batch(raw_batch)
         return self
 
     def __next__(self):
         if not self.buffer:
-            self.buffer = self.fetch_batch()
+            raw_batch = self.fetch_batch()
+            self.buffer = self.transform_batch(raw_batch)
         if not self.buffer:
             raise StopIteration()
         return self.buffer.pop(0)
@@ -48,7 +54,20 @@ class TapGenerator:
             path=self.endpoint_config.get('path'),
             version=self.endpoint_config.get('api_version', 'v2'),
             apikey_type=self.endpoint_config.get('apikey_type', None),
-            params=self.params,
+            params='&'.join([f'{key}={value}' for (key, value) in self.params.items()]),
             endpoint=self.stream_name,
             json=self.endpoint_config.get('body', None))
+        self.time_extracted = utils.now()
+        if isinstance(response, dict):
+            return [response]
         return response
+
+    def transform_batch(self, batch):
+        data_key = self.endpoint_config.get('data_key', None)
+        transformed_batch = list()
+        if data_key is None:
+            transformed_batch = transform_json(batch, self.stream_name)
+        elif data_key in batch:
+            transformed_batch = transform_json(batch, data_key)[data_key]
+        return transformed_batch
+
--- tap_mambu/tap_mambu_refactor/TapProcessors/processor.py
+++ tap_mambu/tap_mambu_refactor/TapProcessors/processor.py
@@ -1,3 +1,9 @@
+import json
+
+
 class TapProcessor:
     def __init__(self):
         pass
+
+    def process(self, record):
+        print(json.dumps(record, indent=2))
