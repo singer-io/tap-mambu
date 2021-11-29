8531705125bbeb5c5ecb7f82b820ac9cf162ba32
Radu Marinoiu <radu.marinoiu@mambu.com>
2021-11-29 16:20:23 +0100

    Finished implementation of Loan Accounts Generator/Processor

--- tap_mambu/tap_mambu_refactor/TapProcessors/processor.py
+++ tap_mambu/tap_mambu_refactor/TapProcessors/processor.py
@@ -1,19 +1,21 @@
 import json
+from abc import ABC
 from typing import List
 
 from singer import write_record, Transformer, metadata, write_schema
 from singer.utils import strptime_to_utc
 
-from ..Helpers import transform_datetime, convert
+from ..Helpers import transform_datetime, convert, write_bookmark
 from ..TapGenerators.generator import TapGenerator
 
 
-class TapProcessor:
+class TapProcessor(ABC):
     def __init__(self, generators: List[TapGenerator], catalog, stream_name):
         self.generators = generators
         self.generator_values = dict()
         self.catalog = catalog
         self.stream_name = stream_name
+        self.deduplication_key = "encoded_key"
         self.stream = self.catalog.get_stream(stream_name)
         self.schema = self.stream.schema.to_dict()
         self.stream_metadata = metadata.to_map(self.stream.metadata)
@@ -28,6 +30,7 @@ class TapProcessor:
         for generator in self.generators:
             self.generator_values[iter(generator)] = None
         while True:
+            # Populate list of values from generators (if any were removed)
             for iterator in list(self.generator_values.keys()):
                 if self.generator_values[iterator] is None:
                     self.generator_values[iterator] = next(iterator, None)
@@ -35,33 +38,36 @@ class TapProcessor:
                     self.generator_values.pop(iterator)
             if not self.generator_values:
                 break
-
+            # Find lowest value in the list
             min_record_key: TapGenerator = None
             min_record_value = None
             for iterator in self.generator_values.keys():
-                new_bookmark_field = convert(iterator.bookmark_field)
                 if min_record_value is None \
-                        or transform_datetime(min_record_value) > transform_datetime(self.generator_values[iterator][new_bookmark_field]):
+                        or min_record_value > self.generator_values[iterator][self.deduplication_key]:
                     min_record_key = iterator
-                    min_record_value = self.generator_values[iterator][new_bookmark_field]
+                    min_record_value = self.generator_values[iterator][self.deduplication_key]
 
+            # Process the record
             record = self.generator_values[min_record_key]
-            record["generator"] = list(self.generator_values.keys()).index(min_record_key)
             self.process_record(record, min_record_key.time_extracted)
 
+            # Remove any record with the same deduplication_key from the list
+            # (so we don't process the same record twice)
             for iterator in self.generator_values.keys():
-                if self.generator_values[min_record_key] == self.generator_values[iterator]:
+                if min_record_value == self.generator_values[iterator][self.deduplication_key]:
                     self.generator_values[iterator] = None
+
+        self.generators[0].write_bookmark()
 
     def __is_record_past_bookmark(self, transformed_record):
         is_record_past_bookmark = False
         bookmark_type = self.generators[0].endpoint_config.get('bookmark_type')
-        bookmark_field = self.generators[0].endpoint_config.get('bookmark_field')
+        bookmark_field = convert(self.generators[0].endpoint_config.get('bookmark_field'))
 
         # Reset max_bookmark_value to new value if higher
         if bookmark_field and (bookmark_field in transformed_record):
             bookmark_dttm = strptime_to_utc(transformed_record[bookmark_field])
-            if self.generators[0].max_bookmark_value:
+            if hasattr(self.generators[0], "max_bookmark_value"):
                 max_bookmark_value_dttm = strptime_to_utc(self.generators[0].max_bookmark_value)
                 if bookmark_dttm > max_bookmark_value_dttm:
                     self.generators[0].max_bookmark_value = transformed_record[bookmark_field]
@@ -74,7 +80,7 @@ class TapProcessor:
                 if transformed_record[bookmark_field] >= self.generators[0].last_bookmark_value:
                     is_record_past_bookmark = True
             elif bookmark_type == 'datetime':
-                last_dttm = transform_datetime(self.generator.last_bookmark_value)
+                last_dttm = transform_datetime(self.generators[0].last_bookmark_value)
                 bookmark_dttm = transform_datetime(transformed_record[bookmark_field])
                 # Keep only records whose bookmark is after the last_datetime
                 if bookmark_dttm >= last_dttm:
