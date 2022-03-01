4a6bc0db316e139477360cbdfe67bbffad7fd166
Alexandru Rosca <alexandru.rosca@mambu.com>
2022-03-01 10:20:39 +0200

    Fixed Branches and Centres inherited processor class

--- tap_mambu/tap_mambu_refactor/tap_processors/branches_processor.py
+++ tap_mambu/tap_mambu_refactor/tap_processors/branches_processor.py
@@ -1,7 +1,7 @@
-from .parent_processor import ParentProcessor
+from .parent_processor import TapProcessor
 
 
-class BranchesProcessor(ParentProcessor):
+class BranchesProcessor(TapProcessor):
     def _init_endpoint_config(self):
         super(BranchesProcessor, self)._init_endpoint_config()
         self.endpoint_deduplication_key = "id"
--- tap_mambu/tap_mambu_refactor/tap_processors/centres_processor.py
+++ tap_mambu/tap_mambu_refactor/tap_processors/centres_processor.py
@@ -1,7 +1,7 @@
-from .parent_processor import ParentProcessor
+from .parent_processor import TapProcessor
 
 
-class CentresProcessor(ParentProcessor):
+class CentresProcessor(TapProcessor):
     def _init_endpoint_config(self):
         super(CentresProcessor, self)._init_endpoint_config()
         self.endpoint_deduplication_key = "id"
