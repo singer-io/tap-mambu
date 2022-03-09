d668fb5f4fe9f5680eb78018a9a9a350c1ba3ebe
Radu Marinoiu <radu.marinoiu@mambu.com>
2022-03-09 12:11:04 +0200

    Removed redundant setting of deduplication key (same as default value)

--- tap_mambu/tap_mambu_refactor/tap_processors/index_rate_sources_processor.py
+++ tap_mambu/tap_mambu_refactor/tap_processors/index_rate_sources_processor.py
@@ -4,4 +4,3 @@ from .parent_processor import TapProcessor
 class IndexRateSourcesProcessor(TapProcessor):
     def _init_endpoint_config(self):
         super(IndexRateSourcesProcessor, self)._init_endpoint_config()
-        self.endpoint_deduplication_key = "encoded_key"
