272b7a3e8697f3e921e89cfc56d85094cde893a4
Alexandru Rosca <alexandru.rosca@mambu.com>
2022-03-11 18:45:26 +0200

    Refactored loan_transactions stream

--- tap_mambu/tap_mambu_refactor/main.py
+++ tap_mambu/tap_mambu_refactor/main.py
@@ -7,6 +7,7 @@ from .tap_generators.deposit_accounts_generator import DepositAccountsGenerator
 from .tap_generators.deposit_cards_generator import DepositCardsGenerator
 from .tap_generators.loan_accounts_generator import LoanAccountsADGenerator, LoanAccountsLMGenerator
 from .tap_generators.loan_repayments_generator import LoanRepaymentsGenerator
+from .tap_generators.loan_transactions_generator import LoanTransactionsGenerator
 from .tap_processors.branches_processor import BranchesProcessor
 from .tap_processors.centres_processor import CentresProcessor
 from .tap_processors.clients_processor import ClientsProcessor
@@ -14,6 +15,7 @@ from .tap_processors.deposit_accounts_processor import DepositAccountsProcessor
 from .tap_processors.deposit_cards_processor import DepositCardsProcessor
 from .tap_processors.loan_accounts_processor import LoanAccountsProcessor
 from .tap_processors.loan_repayments_processor import LoanRepaymentsProcessor
+from .tap_processors.loan_transactions_processor import LoanTransactionsProcessor
 
 LOGGER = singer.get_logger()
 
@@ -28,6 +30,7 @@ def sync_endpoint_refactor(client, catalog, state,
         "deposit_accounts": ((DepositAccountsGenerator,), DepositAccountsProcessor),
         "loan_accounts": ((LoanAccountsLMGenerator, LoanAccountsADGenerator), LoanAccountsProcessor),
         "loan_repayments": ((LoanRepaymentsGenerator,), LoanRepaymentsProcessor),
+        "loan_transactions": ((LoanTransactionsGenerator,), LoanTransactionsProcessor),
     }
 
     generator_classes, processor_class = stream_generator_processor_dict[stream_name]
