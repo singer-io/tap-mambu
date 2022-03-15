d1ebfdc3254c04d040224d989bea273597acb20a
Radu Marinoiu <radu.marinoiu@mambu.com>
2022-03-15 10:26:19 +0200

    Fixed unit tests for installments

--- tap_mambu/tap_mambu_refactor/tap_processors/installments_processor.py
+++
@@ -1,6 +0,0 @@
-from .parent_processor import TapProcessor
-
-
-class InstallmentsProcessor(TapProcessor):
-    def _init_endpoint_config(self):
-        super(InstallmentsProcessor, self)._init_endpoint_config()
