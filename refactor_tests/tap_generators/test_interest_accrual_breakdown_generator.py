25669664518d4b17c9eeac298610ae2eeb350997
Alexandru Rosca <alexandru.rosca@mambu.com>
2022-03-23 17:55:09 +0200

    Fixed the tests after merging the release branch

--- refactor_tests/tap_generators/test_interest_accrual_breakdown_generator.py
+++ refactor_tests/tap_generators/test_interest_accrual_breakdown_generator.py
@@ -1,19 +1,11 @@
-from unittest.mock import MagicMock
-
-from ..constants import config_json
+from . import setup_generator_base_test
 
 
 def test_interest_accrual_breakdown_generator_endpoint_config_init():
-    from tap_mambu.tap_mambu_refactor.tap_generators.interest_accrual_breakdown_generator import \
-        InterestAccrualBreakdownGenerator
-    client_mock = MagicMock()
-    client_mock.page_size = int(config_json.get("page_size", 500))
-    client_mock.request = MagicMock()
-    generator = InterestAccrualBreakdownGenerator(stream_name="interest_accrual_breakdown",
-                                                  client=client_mock,
-                                                  config=config_json,
-                                                  state={'currently_syncing': 'interest_accrual_breakdown'},
-                                                  sub_type="self")
+    generators = setup_generator_base_test("interest_accrual_breakdown")
+
+    assert 1 == len(generators)
+    generator = generators[0]
 
     assert generator.endpoint_path == 'accounting/interestaccrual:search'
     assert generator.endpoint_bookmark_field == "creationDate"
--- refactor_tests/tap_processors/test_interest_accrual_breakdown_processor.py
+++ refactor_tests/tap_processors/test_interest_accrual_breakdown_processor.py
@@ -1,26 +1,7 @@
-import inspect
-import os
-
-from mock import MagicMock
-
-from ..constants import config_json
-from ..helpers import GeneratorMock
-
-FIXTURES_PATH = f"{os.path.dirname(os.path.abspath(inspect.stack()[0][1]))}/Fixtures"
+from . import setup_processor_base_test
 
 
 def test_interest_accrual_breakdown_processor():
-    from singer.catalog import Catalog
-    catalog = Catalog.load(f"{FIXTURES_PATH}/processor_catalog.json")
-    client_mock = MagicMock()
-
-    from tap_mambu.tap_mambu_refactor.tap_processors.interest_accrual_breakdown import InterestAccrualBreakdownProcessor
-    processor = InterestAccrualBreakdownProcessor(catalog=catalog,
-                                                  stream_name="interest_accrual_breakdown",
-                                                  client=client_mock,
-                                                  config=config_json,
-                                                  state={'currently_syncing': 'interest_accrual_breakdown'},
-                                                  sub_type="self",
-                                                  generators=[GeneratorMock([])])
+    processor = setup_processor_base_test("interest_accrual_breakdown")
 
     assert processor.endpoint_deduplication_key == "entry_id"
