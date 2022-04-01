b96009fd71d00a449e0eea5eb849a752d2953c5a
Radu Marinoiu <radu.marinoiu@mambu.com>
2022-04-01 12:52:49 +0300

    Removed sorting from deposit products as it's not present in the v2

--- refactor_tests/tap_generators/test_deposit_products_generator.py
+++ refactor_tests/tap_generators/test_deposit_products_generator.py
@@ -10,5 +10,4 @@ def test_deposit_products_generator_endpoint_config_init():
 
     assert generator.endpoint_path == "savingsproducts"
     assert generator.endpoint_api_method == "GET"
-    assert generator.endpoint_params.get("sortBy") == "id:ASC"
     assert generator.endpoint_bookmark_field == "lastModifiedDate"
--- tap_mambu/tap_mambu_refactor/tap_generators/deposit_products_generator.py
+++ tap_mambu/tap_mambu_refactor/tap_generators/deposit_products_generator.py
@@ -5,7 +5,6 @@ from ..helpers import transform_datetime, get_bookmark
 class DepositProductsGenerator(TapGenerator):
     def _init_endpoint_config(self):
         super(DepositProductsGenerator, self)._init_endpoint_config()
-        self.endpoint_path = "savingsproducts"
+        self.endpoint_path = "depositproducts"
         self.endpoint_api_method = "GET"
-        self.endpoint_params["sortBy"] = "id:ASC"
         self.endpoint_bookmark_field = "lastModifiedDate"
