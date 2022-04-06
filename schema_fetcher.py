f87eefb2f45eb835be3ba6d6bba2ba9e9d03eb57
Alexandru Rosca <alexandru.rosca@mambu.com>
2022-04-06 13:42:13 +0300

    Fixed some edge cases

--- schema_fetcher.py
+++ schema_fetcher.py
@@ -71,6 +71,19 @@ class Streams:
             streams_singular_form[idx] = self.__swaggered_streams.get(val, val)
         return streams_singular_form
 
+    def convert_swaggered_to_tap_stream(self, swaggered_stream):
+        if swaggered_stream in self.__swaggered_streams.values():
+            for tap_stream, swaggered in self.__swaggered_streams.items():
+                if swaggered == swaggered_stream:
+                    return tap_stream
+        return swaggered_stream
+
+    def get_tap_streams_count(self):
+        return len(self.__tap_streams_singular_form)
+
+    def get_tap_streams_name(self):
+        return self.__tap_streams_singular_form.keys()
+
 
 def get_mambu_client():
     config = load_json(CLIENT_CONFIG_FILE_PATH)
@@ -111,8 +124,11 @@ TAP_FORMATS = {
 }
 
 
-def get_data_type_and_format(field_type, field_format):
+def get_data_type_and_format(field_type, field_format=None, obj_type_nullable=True):
     tap_field_type = TAP_TYPES_AND_FORMATS[field_type]
+    if not obj_type_nullable and isinstance(tap_field_type, list):
+        tap_field_type = tap_field_type[1]
+
     type_and_format = {'type': tap_field_type}
     if field_format and TAP_FORMATS[field_format]:
         type_and_format['format'] = TAP_FORMATS[field_format]
@@ -128,10 +144,12 @@ def get_stream_resource_file_paths():
     file_paths = []
     found_streams_swagger_format = []
     swagger_format_streams = []
+
+    tap_streams_swaggered = streams.get_stream_names_swaggered()
     for file_data in available_resources['items']:
         stream_name = file_data['hashValue'].lower()
         swagger_format_streams.append(stream_name)
-        if stream_name in streams.get_stream_names_swaggered():
+        if stream_name in tap_streams_swaggered:
             found_streams_swagger_format.append(stream_name)
             file_paths.append(file_data['jsonPath'])
 
@@ -143,8 +161,10 @@ def get_stream_resource_file_paths():
     return zip(found_streams_swagger_format, file_paths)
 
 
-def generate_json_objs(field_properties, all_schema_obj):
-    full_schema = {'type': 'object', 'additionalProperties': False, 'properties': {}}
+def generate_json_objs(field_properties, all_schema_obj, type_object_nullable=True):
+    full_schema = {**get_data_type_and_format('object', obj_type_nullable=type_object_nullable),
+                   'additionalProperties': False,
+                   'properties': {}}
 
     for field_name, field_prop in field_properties['properties'].items():
         field_name = convert_pascal_to_snake(field_name)
@@ -157,11 +177,13 @@ def generate_json_objs(field_properties, all_schema_obj):
             if 'originalRef' in field_prop['items']:
                 full_schema['properties'][field_name]['anyOf'][0]['items'] = generate_json_objs(
                     all_schema_obj[field_prop['items']['originalRef']],
-                    all_schema_obj)
+                    all_schema_obj,
+                    False)
             else:
                 full_schema['properties'][field_name]['anyOf'][0]['items'] = get_data_type_and_format(
-                    field_prop['type'],
-                    field_prop.get('format', None))
+                    field_prop['items']['type'],
+                    field_prop.get('format', None),
+                    obj_type_nullable=False)
         else:
             if 'originalRef' in field_prop:
                 full_schema['properties'][field_name] = generate_json_objs(all_schema_obj[field_prop['originalRef']],
@@ -179,14 +201,23 @@ def generate_json_schema(stream_name, file_path):
 
     stream_fields_w_refs = stream_data['definitions']
     stream_fields = stream_fields_w_refs[convert_snake_to_pascal(streams.get_stream_json_obj_form(stream_name))]
-    return generate_json_objs(stream_fields, stream_fields_w_refs)
+    return generate_json_objs(stream_fields, stream_fields_w_refs, False)
 
 
 def main():
     for stream_name, file_path in get_stream_resource_file_paths():
-        file_dir_path = os.path.join(OUTPUT_DIR_PATH, f'{stream_name}.json')
+        file_dir_path = os.path.join(OUTPUT_DIR_PATH, f'{streams.convert_swaggered_to_tap_stream(stream_name)}.json')
         with open(file_dir_path, 'w') as f:
-            json.dump(generate_json_schema(stream_name, file_path), f, indent=2)
+            json.dump(generate_json_schema(stream_name, file_path),
+                      f,
+                      indent=2)
+
+    created_files = [file_name for file_name in os.listdir(OUTPUT_DIR_PATH) if os.path.isfile(file_name)]
+    if len(created_files) != streams.get_tap_streams_count():
+        created_files_name_only = [created_file.replace('.json', '') for created_file in created_files]
+        for tap_stream in streams.get_tap_streams_name():
+            if tap_stream not in created_files_name_only:
+                raise FileNotFoundError(f'Json Schema for "{tap_stream}" stream not found')
 
 
 if __name__ == '__main__':
