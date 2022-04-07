ea05702effbb66b7d849287449b40c5e7119f808
Radu Marinoiu <radu.marinoiu@mambu.com>
2022-04-07 16:29:41 +0300

    Fixed unit test

--- refactor_tests/tap_generators/test_deposit_products_generator.py
+++ refactor_tests/tap_generators/test_deposit_products_generator.py
@@ -8,6 +8,6 @@ def test_deposit_products_generator_endpoint_config_init():
 
     generator = generators[0]
 
-    assert generator.endpoint_path == "savingsproducts"
+    assert generator.endpoint_path == "depositproducts"
     assert generator.endpoint_api_method == "GET"
     assert generator.endpoint_bookmark_field == "lastModifiedDate"
