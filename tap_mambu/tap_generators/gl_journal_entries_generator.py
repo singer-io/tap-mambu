9312761515528fec956fa26ceb4444e81481395a
Alexandru Rosca <alexandru.rosca@mambu.com>
2022-05-17 17:06:19 +0300

    Added multithreading to gl_journal_entries stream

--- tap_mambu/tap_generators/gl_journal_entries_generator.py
+++ tap_mambu/tap_generators/gl_journal_entries_generator.py
@@ -1,15 +1,14 @@
 from singer import utils
 
-from .generator import TapGenerator
+from .multithreaded_bookmark_generator import MultithreadedBookmarkGenerator
 from ..helpers import get_bookmark
 from ..helpers.datetime_utils import str_to_localized_datetime, datetime_to_utc_str
 
 
-class GlJournalEntriesGenerator(TapGenerator):
+class GlJournalEntriesGenerator(MultithreadedBookmarkGenerator):
     def _init_endpoint_config(self):
         super(GlJournalEntriesGenerator, self)._init_endpoint_config()
         self.endpoint_path = "gljournalentries:search"
-        self.endpoint_api_method = "POST"
         self.endpoint_bookmark_field = "creationDate"
         self.endpoint_sorting_criteria = {
             "field": "entryId",
@@ -24,3 +23,7 @@ class GlJournalEntriesGenerator(TapGenerator):
                 "secondValue": utils.now().strftime("%Y-%m-%d")[:10]
             }
         ]
+
+    def prepare_batch_params(self):
+        super(GlJournalEntriesGenerator, self).prepare_batch_params()
+        self.endpoint_filter_criteria[0]["value"] = self.endpoint_intermediary_bookmark_value
