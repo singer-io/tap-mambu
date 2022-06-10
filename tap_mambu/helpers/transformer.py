7213d9b8b6ae4c896817f12a547261c08991430f
Radu Marinoiu <radu.marinoiu@mambu.com>
2022-06-10 16:39:01 +0300

    Added logging back to requests, removed leftover file from another task

--- tap_mambu/helpers/transformer.py
+++
@@ -1,8 +0,0 @@
-from singer.transform import Transformer as SingerTransformer
-
-from tap_mambu.helpers.datetime_utils import datetime_to_utc_str, str_to_localized_datetime
-
-
-class Transformer(SingerTransformer):
-    def _transform_datetime(self, value):
-        return datetime_to_utc_str(str_to_localized_datetime(value))
