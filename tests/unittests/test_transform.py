f613d9ad310fbe4e7ea424cea56f85a5fc7480c3
Leslie VanDeMark <lvandemark@talend.com>
2023-02-21 20:07:17 +0000

    update tests with new custom field set format

--- tests/unittests/test_transform.py
+++ tests/unittests/test_transform.py
@@ -5,18 +5,16 @@ from tap_mambu.helpers import transform_json
 class TestTransformJson(unittest.TestCase):
 
     def test_transform_json_handles_dictionary_custom_fields(self):
-        expected = [{'custom_fields': [{'field_set_id': '_custom_1', 'id': 'id', 'value': '1'}],
+        expected = [{'custom_fields': [{"_custom_1" : {"id": '1'}}],
                      'id': '1'}]
         actual = transform_json([{"_custom_1" : {"id": '1'}, "id": '1'}],
                                 "my_path")
         self.assertEqual(expected, actual)
 
     def test_transform_json_handles_list_custom_fields(self):
-        expected = [{'custom_fields': [{'field_set_id': '_custom_1', 'id': 'id', 'value': '1'},
-                                       {'field_set_id': '_custom_2', 'id': 'id', 'value': '2'},
-                                       {'field_set_id': '_custom_2', 'id': 'index', 'value': '0'},
-                                       {'field_set_id': '_custom_2', 'id': 'id', 'value': '3'},
-                                       {'field_set_id': '_custom_2', 'id': 'index', 'value': '1'}],
+        expected = [{'custom_fields': [{"_custom_1" : {"id": '1'},
+                                        "_custom_2" : [{"id": '2', "index": '0'},
+                                                       {"id": '3', "index": '1'}],
                      'id': '1'}]
 
         actual = transform_json([{"_custom_1" : {"id": '1'},
