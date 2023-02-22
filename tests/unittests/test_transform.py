97a3f43e771cf1f9e42a9a5f686c86f509dbd54d
Leslie VanDeMark <lvandemark@talend.com>
2023-02-22 15:54:49 +0000

    update tests

--- tests/unittests/test_transform.py
+++ tests/unittests/test_transform.py
@@ -5,25 +5,29 @@ from tap_mambu.helpers import transform_json
 class TestTransformJson(unittest.TestCase):
 
     def test_transform_json_handles_dictionary_custom_fields(self):
-        expected = [{'custom_fields': [{"_custom_1" : {"id": '1'}}],
-                     'id': '1'}]
+        expected = [{'_custom_1': {'id': '1'},
+                     'id': '1',
+                     'custom_fields': [{'_custom_1': {'id': '1'}}]}]
         actual = transform_json([{"_custom_1" : {"id": '1'}, "id": '1'}],
                                 "my_path")
         self.assertEqual(expected, actual)
 
     def test_transform_json_handles_list_custom_fields(self):
-        expected = [{'custom_fields': [{"_custom_1" : {"id": '1'},
-                                        "_custom_2" : [{"id": '2', "index": '0'},
-                                                       {"id": '3', "index": '1'}],
-                                        }],
-                     'id': '1'}]
-
+        # the `_` fields will be stripped by the transformer
+        expected = [{'_custom_1': {'id': '1'},
+                     '_custom_2': [{'id': '2', 'index': '0'},
+                                   {'id': '3', 'index': '1'}],
+                     'id': '1',
+                     'custom_fields': [{'_custom_1': {'id': '1'}},
+                                       {'_custom_2': [{'id': '2', 'index': '0'},
+                                                      {'id': '3', 'index': '1'}]}
+                                        ]
+                     }]
         actual = transform_json([{"_custom_1" : {"id": '1'},
                                   "_custom_2" : [{"id": '2', "index": '0'},
                                                  {"id": '3', "index": '1'}],
                                   "id": '1'}],
                                 "my_path")
-
         self.assertEqual(expected, actual)
 
     def test_transform_no_custom_fields(self):
@@ -31,4 +35,5 @@ class TestTransformJson(unittest.TestCase):
                      'id': '1'}]
         actual = transform_json([{"id": '1'}],
                                 "my_path")
+
         self.assertEquals(expected, actual)
