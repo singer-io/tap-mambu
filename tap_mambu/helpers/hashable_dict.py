f9f186b851128ae16bde59bd70200272b1be737d
bryantgray <bgray@talend.com>
2024-01-16 11:03:44 -0500

    Handle `None` values in `HashableDict` (#109)
    * Adding None support to HashableDict
    
    * Handle `None` in `HashableDict`
    
    * Bump version and update changelog
    
    ---------
    
    Co-authored-by: Collin Simon <csimon@talend.com>

--- tap_mambu/helpers/hashable_dict.py
+++ tap_mambu/helpers/hashable_dict.py
@@ -1,6 +1,3 @@
-import json
-
-
 class HashableDict(dict):
 
     @staticmethod
@@ -8,7 +5,11 @@ class HashableDict(dict):
         if type(value) in [dict, HashableDict]:
             return HashableDict(value).__key()
         if type(value) == list:
-            return tuple(sorted(map(HashableDict._recur_hash, value)))
+            # None values are not sortable, but they are appended to the end of
+            # the list so that they are still hashed
+            sortable_values = [x for x in map(HashableDict._recur_hash, value) if x is not None]
+            none_values = [x for x in map(HashableDict._recur_hash, value) if x is None]
+            return tuple(sorted(sortable_values) + none_values)
         return value
 
     def __key(self):
