5893b1de6bcb736a41387eee4306dde187dc949d
shantanu73 <shantanudhiman25@gmail.com>
2024-01-31 07:02:13 +0000

    Convert Clients stream to FULL sync stream.

--- tap_mambu/tap_generators/clients_generator.py
+++ tap_mambu/tap_generators/clients_generator.py
@@ -1,30 +1,8 @@
-from .multithreaded_bookmark_generator import MultithreadedBookmarkGenerator
-from ..helpers import get_bookmark
-from ..helpers.datetime_utils import str_to_localized_datetime, datetime_to_local_str
+from .generator import TapGenerator
 
 
-class ClientsGenerator(MultithreadedBookmarkGenerator):
-    def _init_config(self):
-        super()._init_config()
-        self.max_threads = 10
-
+class ClientsGenerator(TapGenerator):
     def _init_endpoint_config(self):
         super(ClientsGenerator, self)._init_endpoint_config()
-        self.endpoint_path = "clients:search"
-        self.endpoint_bookmark_field = "lastModifiedDate"
-        self.endpoint_sorting_criteria = {
-            "field": "lastModifiedDate",
-            "order": "ASC"
-        }
-        self.endpoint_filter_criteria = [
-            {
-                "field": "lastModifiedDate",
-                "operator": "AFTER",
-                "value": datetime_to_local_str(str_to_localized_datetime(
-                    get_bookmark(self.state, self.stream_name, self.sub_type, self.start_date)))
-            }
-        ]
-
-    def prepare_batch_params(self):
-        super(ClientsGenerator, self).prepare_batch_params()
-        self.endpoint_filter_criteria[0]["value"] = datetime_to_local_str(self.endpoint_intermediary_bookmark_value)
+        self.endpoint_path = "clients"
+        self.endpoint_api_method = "GET"
