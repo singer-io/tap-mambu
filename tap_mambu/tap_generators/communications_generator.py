554720b520db1c0a65d9d870b83e616b12f708cc
shantanu73 <shantanudhiman25@gmail.com>
2024-03-22 09:32:43 +0000

    Implemented modify_request_params method for Communications stream.

--- tap_mambu/tap_generators/communications_generator.py
+++ tap_mambu/tap_generators/communications_generator.py
@@ -1,6 +1,5 @@
 from .multithreaded_bookmark_generator import MultithreadedBookmarkGenerator
-from ..helpers import get_bookmark
-from ..helpers.datetime_utils import datetime_to_local_str, str_to_localized_datetime
+from ..helpers.datetime_utils import datetime_to_local_str
 
 
 class CommunicationsGenerator(MultithreadedBookmarkGenerator):
@@ -12,23 +11,18 @@ class CommunicationsGenerator(MultithreadedBookmarkGenerator):
             "paginationDetails": "OFF"
         }
         self.endpoint_bookmark_field = "creationDate"
-        self.endpoint_filter_criteria = [
-            {
-                "field": "state",
-                "operator": "EQUALS",
-                "value": "SENT"
-            },
-            {
-                "field": "creationDate",
-                "operator": "AFTER",
-                "value": datetime_to_local_str(str_to_localized_datetime(
-                    get_bookmark(self.state, self.stream_name, self.sub_type, self.start_date)))
-            }
-        ]
+
+    def modify_request_params(self, start, end):
+        super().modify_request_params(start, end)
+        self.endpoint_body['filterCriteria'].append({
+            "field": "state",
+            "operator": "EQUALS",
+            "value": "SENT"
+        })
 
     def _init_endpoint_body(self):
         self.endpoint_body = self.endpoint_filter_criteria
 
     def prepare_batch_params(self):
         super(CommunicationsGenerator, self).prepare_batch_params()
-        self.endpoint_filter_criteria[1]["value"] = datetime_to_local_str(self.endpoint_intermediary_bookmark_value)
+        self.endpoint_filter_criteria[0]["value"] = datetime_to_local_str(self.endpoint_intermediary_bookmark_value)
