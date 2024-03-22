70fc6165382f7e32d4683b96d59a8f064cca18d6
shantanu73 <shantanudhiman25@gmail.com>
2024-03-22 09:38:33 +0000

    Removed endpoint filter criteria for Interest Accrual Breakdown stream.

--- tap_mambu/tap_generators/interest_accrual_breakdown_generator.py
+++ tap_mambu/tap_generators/interest_accrual_breakdown_generator.py
@@ -1,6 +1,5 @@
 from .multithreaded_bookmark_generator import MultithreadedBookmarkDayByDayGenerator
-from ..helpers import get_bookmark
-from ..helpers.datetime_utils import datetime_to_utc_str, str_to_localized_datetime
+from ..helpers.datetime_utils import datetime_to_utc_str
 
 
 class InterestAccrualBreakdownGenerator(MultithreadedBookmarkDayByDayGenerator):
@@ -12,14 +11,6 @@ class InterestAccrualBreakdownGenerator(MultithreadedBookmarkDayByDayGenerator):
             "field": "entryId",
             "order": "ASC"
         }
-        self.endpoint_filter_criteria = [
-            {
-                "field": "creationDate",
-                "operator": "AFTER",
-                "value": datetime_to_utc_str(str_to_localized_datetime(
-                    get_bookmark(self.state, self.stream_name, self.sub_type, self.start_date)))[:10]
-            }
-        ]
 
     def prepare_batch_params(self):
         super(InterestAccrualBreakdownGenerator, self).prepare_batch_params()
