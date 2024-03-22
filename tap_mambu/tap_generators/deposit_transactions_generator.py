11e44de095810de4a940d60a204ba62cbdf57def
shantanu73 <shantanudhiman25@gmail.com>
2024-03-22 09:35:14 +0000

    Removed endpoint filter criteria for DepositTransactions stream.

--- tap_mambu/tap_generators/deposit_transactions_generator.py
+++ tap_mambu/tap_generators/deposit_transactions_generator.py
@@ -1,6 +1,5 @@
 from .multithreaded_bookmark_generator import MultithreadedBookmarkGenerator
-from ..helpers import get_bookmark
-from ..helpers.datetime_utils import datetime_to_utc_str, str_to_localized_datetime
+from ..helpers.datetime_utils import datetime_to_utc_str
 
 
 class DepositTransactionsGenerator(MultithreadedBookmarkGenerator):
@@ -13,14 +12,6 @@ class DepositTransactionsGenerator(MultithreadedBookmarkGenerator):
         self.endpoint_path = "deposits/transactions:search"
         self.endpoint_bookmark_field = "creationDate"
         self.endpoint_sorting_criteria["field"] = "id"
-        self.endpoint_filter_criteria = [
-            {
-                "field": "creationDate",
-                "operator": "AFTER",
-                "value": datetime_to_utc_str(str_to_localized_datetime(
-                    get_bookmark(self.state, self.stream_name, self.sub_type, self.start_date)))
-            }
-        ]
 
     def prepare_batch_params(self):
         super(DepositTransactionsGenerator, self).prepare_batch_params()
