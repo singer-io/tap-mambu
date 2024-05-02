0aabdafce1163d0564e9edd25430c2720e110497
Rushikesh Todkar <98420315+RushiT0122@users.noreply.github.com>
2024-05-02 20:27:24 +0530

    Reduce integration test execution time

--- tests/base.py
+++ tests/base.py
@@ -246,7 +246,7 @@ class MambuBaseTest(unittest.TestCase):
 
     def get_properties(self, original_properties=True):
         properties = {
-            'start_date': '2017-01-01T00:00:00Z',
+            'start_date': '2021-01-01T00:00:00Z',
             'username': os.environ['TAP_MAMBU_USERNAME'],
             'subdomain': os.environ['TAP_MAMBU_SUBDOMAIN'],
             'page_size': '100'
