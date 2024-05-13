da2f222f38b023695380940e08fa6e3de2a99819
shantanu73 <shantanudhiman25@gmail.com>
2024-05-13 08:07:44 +0000

    Bump version 4.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-mambu',
-      version='4.1.0',
+      version='4.2.0',
       description='Singer.io tap for extracting data from the Mambu 2.0 API',
       author='jeff.huth@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
