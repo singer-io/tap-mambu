b003f4333c05b0c9dbbcad38cfcecd8781d22d10
rumone.anderson <rumone.andeson@cornerstone.com.jm>
2025-03-10 17:54:48 -0500

    override transform method instead

--- tap_mambu/tap_generators/loan_repayments_generator.py
+++ tap_mambu/tap_generators/loan_repayments_generator.py
@@ -19,37 +19,12 @@ class LoanRepaymentsGenerator(ChildGenerator):
         self.endpoint_api_method = "GET"
         # include parent id in endpoint path
         self.endpoint_path = f"loans/{self.endpoint_parent_id}/schedule"
-    
-    # Override the Generator method to retrieve schedule information from mambu apis
-    def fetch_batch(self):
-        endpoint_querystring = '&'.join([f'{key}={value}' for (key, value) in self.params.items()])
 
-        LOGGER.info(f'(generator) Stream {self.stream_name} - URL for {self.stream_name} ({self.endpoint_api_method}, '
-                    f'{self.endpoint_api_version}): {self.client.base_url}/{self.endpoint_path}?{endpoint_querystring}')
-        LOGGER.info(f'(generator) Stream {self.stream_name} - body = {self.endpoint_body}')
+    def transform_batch(self, batch):
+        batch_installments = batch['installments']
+        batch_currency = batch['currency']
 
-        response = self.client.request(
-            method=self.endpoint_api_method,
-            path=self.endpoint_path,
-            version=self.endpoint_api_version,
-            apikey_type=self.endpoint_api_key_type,
-            params=endpoint_querystring,
-            endpoint=self.stream_name,
-            json=self.endpoint_body
-        )
+        for installment in batch_installments: 
+            installment['currency'] = batch_currency
 
-        response = self._format_response(response)
-
-        self.time_extracted = utils.now()
-        LOGGER.info(f'(generator) Stream {self.stream_name} - extracted records: {len(response)}')
-        return self.transform_batch(response)
-    
-    def _format_response(self, response):  
-        installments = []
-        for installment in response['installments']:
-            installment.update(response['currency'])
-            installments.append(installment)
-        
-        return installments
-        
-        
+        return super(LoanRepaymentsGenerator, self).transform_batch(batch_installments)
