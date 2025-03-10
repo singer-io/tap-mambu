21b21fadba4e647983c9dabefef9213efee4b6c2
rumone.anderson <rumone.andeson@cornerstone.com.jm>
2025-03-10 18:01:35 -0500

    generator defaults to v2 remove unnecessary assignment

--- tap_mambu/tap_generators/loan_repayments_generator.py
+++ tap_mambu/tap_generators/loan_repayments_generator.py
@@ -15,7 +15,6 @@ class LoanRepaymentsGenerator(ChildGenerator):
 
     def _init_endpoint_config(self):
         super(LoanRepaymentsGenerator, self)._init_endpoint_config()
-        self.endpoint_api_version = "v2"
         self.endpoint_api_method = "GET"
         # include parent id in endpoint path
         self.endpoint_path = f"loans/{self.endpoint_parent_id}/schedule"
