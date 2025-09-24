17c402009729af6c0076993d14791465d883a465
prijendev <prijen.khokhani@qlik.com>
2025-09-24 11:29:35 +0530

    Update error message

--- tap_mambu/helpers/datetime_utils.py
+++ tap_mambu/helpers/datetime_utils.py
@@ -36,7 +36,8 @@ def get_timezone_info(client: MambuClient, config_timezone: str = None):
             LOGGER.warning("Could not get timezone information from Mambu endpoint, using config timezone")
             _timezone = timezone(config_timezone)
         else:
-            raise RuntimeError("Unable to retrieve timezone information from the Mambu endpoint. Please provide administrator credentials or configure valid timezone in the UI(e.g., US/Pacific).")
+            raise RuntimeError("Unable to retrieve timezone information from the Mambu endpoint. Please provide administrator credentials or configure valid timezone in the UI(e.g., US/Pacific)." \
+                " Refer this for more details: https://support.mambu.com/docs/organization-contact-currency-and-timezone")
 
 def localize(dttm: datetime) -> datetime:
     if _timezone is None:
