439e9ee41a37abdbe4efdef58e01a1fc44bbd1a3
rsaha-qlik <vpz@qlik.com>
2026-07-27 16:10:53 +0530

    reverted child sync logic changes

--- tap_mambu/tap_processors/child_processor.py
+++ tap_mambu/tap_processors/child_processor.py
@@ -1,13 +1,10 @@
 from .processor import TapProcessor
-from ..helpers.schema import STREAMS
 
 
 class ChildProcessor(TapProcessor):
-    def __init__(self, catalog, stream_name, client, config, state, sub_type, generators,
-                 parent_id, parent_replication_values=None):
+    def __init__(self, catalog, stream_name, client, config, state, sub_type, generators, parent_id):
         super().__init__(catalog, stream_name, client, config, state, sub_type, generators)
         self.endpoint_parent_id = parent_id
-        self.parent_replication_values = parent_replication_values or {}
 
     def _init_endpoint_config(self):
         super()._init_endpoint_config()
@@ -15,7 +12,4 @@ class ChildProcessor(TapProcessor):
 
     def process_record(self, record, time_extracted, bookmark_field):
         record[f'{self.endpoint_parent}_id'] = self.endpoint_parent_id
-        for replication_key in STREAMS.get(self.stream_name, {}).get("replication_keys", []):
-            if replication_key not in record:
-                record[replication_key] = self.parent_replication_values.get(replication_key)
         return super().process_record(record, time_extracted, bookmark_field)
